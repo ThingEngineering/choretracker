745278589764fbc5186bebe200504d8a382b816c
Freddie <freddie@wafflemonster.org>
2023-08-27 18:03:15 -0700

    Draggable frame, ugh

--- Modules/Display.lua
+++ Modules/Display.lua
@@ -12,15 +12,22 @@ function Module:OnInitialize()
         tile = true,
         tileSize = 16,
     })
-    frame:SetBackdropColor(0, 0, 0, 0.8)
-    frame:SetBackdropBorderColor(32/255, 32/255, 32/255, 0.8)
-    frame:SetFrameStrata('MEDIUM')
+    frame:SetBackdropColor(0, 0, 0, 0.6)
+    frame:SetBackdropBorderColor(63/255, 63/255, 63/255, 0.6)
+    -- frame:SetFrameStrata('MEDIUM')
     frame:SetHeight(200)
     frame:SetWidth(400)
     frame:SetPoint('TOPLEFT', 100, -100)
 
+    frame:SetClampedToScreen(true)
+    frame:SetMovable(true)
+    frame:EnableMouse(true)
+    frame:RegisterForDrag('LeftButton')
+    frame:SetScript('OnDragStart', self.OnDragStart)
+    frame:SetScript('OnDragStop', self.OnDragStop)
+
     local text = frame:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
-    text:SetPoint('CENTER')
+    text:SetPoint('TOPLEFT', frame, 'TOPLEFT')
     text:SetText('HI MUM')
 
     self.frame = frame
@@ -30,4 +37,17 @@ end
 
 function Module:OnEnteringWorld()
     self.frame:Show()
+end
+
+function Module:OnDragStart()
+    print('WE STARTING')
+    self:StartMoving()
+end
+
+function Module:OnDragStop()
+    print('WE STOPPING')
+    self:StopMovingOrSizing()
+    local left = self:GetLeft()
+    local top = self:GetTop()
+    print('top: '..top..' | left: '..left)
 end
