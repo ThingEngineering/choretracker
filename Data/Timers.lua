49b7f0cdc62a52df7f88a45922c451d31bd297db
Freddie <freddie@wafflemonster.org>
2024-05-04 15:30:25 -0700

    Associate timers with awakened quests

--- Data/Timers.lua
+++ Data/Timers.lua
@@ -8,18 +8,14 @@ Addon.data.timers = {
         minimumLevel = 60,
         interval = 90 * 60,
         duration = 15 * 60,
-    },
-    {
-        key = 'dreamsurge',
-        minimumLevel = 70,
-        interval = 30 * 60,
-        duration = 5 * 60,
+        awakenedQuest = 80385, -- Last Hurrah: Dragon Isles
     },
     {
         key = 'dragonbaneKeep',
         minimumLevel = 60,
         interval = 120 * 60,
         duration = 15 * 60,
+        awakenedQuest = 80385, -- Last Hurrah: Dragon Isles
     },
     {
         key = 'researchersUnderFire',
@@ -27,17 +23,27 @@ Addon.data.timers = {
         interval = 60 * 60,
         duration = 25 * 60,
         offset = 30 * 60,
-    },
-    {
-        key = 'superbloom',
-        minimumLevel = 70,
-        interval = 60 * 60,
-        duration = 15 * 60,
+        awakenedQuest = 80386, -- Last Hurrah: Zaralek Caverns and Time Rifts
     },
     {
         key = 'timeRift',
         minimumLevel = 60,
         interval = 60 * 60,
         duration = 15 * 60,
+        awakenedQuest = 80386, -- Last Hurrah: Zaralek Caverns and Time Rifts
+    },
+    {
+        key = 'superbloom',
+        minimumLevel = 70,
+        interval = 60 * 60,
+        duration = 15 * 60,
+        awakenedQuest = 80388, -- Last Hurrah: Emerald Dream
+    },
+    {
+        key = 'dreamsurge',
+        minimumLevel = 70,
+        interval = 30 * 60,
+        duration = 5 * 60,
+        awakenedQuest = 80388, -- Last Hurrah: Emerald Dream
     },
 }
