d14c1b941ce43a7ebf05656f88ff578b4405bc90
Freddie <freddie@wafflemonster.org>
2024-08-27 10:30:37 -0700

    Remove awakened timers

--- Data/Timers.lua
+++ Data/Timers.lua
@@ -8,14 +8,12 @@ Addon.data.timers = {
         minimumLevel = 60,
         interval = 90 * 60,
         duration = 15 * 60,
-        awakenedMap = 2522, -- Vault of the Incarnates
     },
     {
         key = 'dragonbaneKeep',
         minimumLevel = 60,
         interval = 120 * 60,
         duration = 15 * 60,
-        awakenedMap = 2522, -- Vault of the Incarnates
     },
     {
         key = 'researchersUnderFire',
@@ -23,28 +21,24 @@ Addon.data.timers = {
         interval = 60 * 60,
         duration = 25 * 60,
         offset = 30 * 60,
-        awakenedMap = 2569, -- Aberrus, the Shadowed Crucible
     },
     {
         key = 'timeRift',
         minimumLevel = 60,
         interval = 60 * 60,
         duration = 15 * 60,
-        awakenedMap = 2569, -- Aberrus, the Shadowed Crucible
     },
     {
         key = 'superbloom',
         minimumLevel = 70,
         interval = 60 * 60,
         duration = 15 * 60,
-        awakenedMap = 2549, -- Amirdrassil, the Dream's Hope
     },
     {
         key = 'dreamsurge',
         minimumLevel = 70,
         interval = 30 * 60,
         duration = 5 * 60,
-        awakenedMap = 2549, -- Amirdrassil, the Dream's Hope
     },
     {
         key = 'bigDig',
