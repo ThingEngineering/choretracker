6630fef0423d395633609f252ef2078db3a8999d
Freddie <freddie@wafflemonster.org>
2024-09-09 18:24:34 -0700

    Change "Worldsoul" chore to "Emissary" and add missing ones

--- Data/Chores/The_War_Within.lua
+++ Data/Chores/The_War_Within.lua
@@ -12,6 +12,50 @@ Addon.data.chores.choresWarWithin = {
             key = 'patch_11_0_0',
             quests = {
                 {
+                    key = 'emissary',
+                    minimumLevel = 70,
+                    chooseQuest = true,
+                    entries = {
+                        { quest = 82678 }, -- Archives: The First Disc
+                        { quest = 82708 }, -- Delves: Nerubian Menace
+                        { quest = 82511 }, -- Worldsoul: Awakening Machine
+                        { quest = 82453 }, -- Worldsoul: Encore!
+                        { quest = 82516 }, -- Worldsoul: Forging a Pact
+                        { quest = 82458 }, -- Worldsoul: Renown
+                        { quest = 82482 }, -- Worldsoul: Snuffling
+                        { quest = 82483 }, -- Worldsoul: Spreading the Light
+                        { quest = 82512 }, -- Worldsoul: World Boss
+                        { quest = 82452 }, -- Worldsoul: World Quests
+                        { quest = 82491 }, -- Worldsoul: Ara-Kara, City of Echoes [N]
+                        { quest = 82494 }, -- Worldsoul: Ara-Kara, City of Echoes [H]
+                        { quest = 82502 }, -- Worldsoul: Ara-Kara, City of Echoes [M]
+                        { quest = 82485 }, -- Worldsoul: Cinderbrew Meadery [N]
+                        { quest = 82495 }, -- Worldsoul: Cinderbrew Meadery [H]
+                        { quest = 82503 }, -- Worldsoul: Cinderbrew Meadery [M]
+                        { quest = 82492 }, -- Worldsoul: City of Threads [N]
+                        { quest = 82496 }, -- Worldsoul: City of Threads [H]
+                        { quest = 82504 }, -- Worldsoul: City of Threads [M]
+                        { quest = 82488 }, -- Worldsoul: Darkflame Cleft [N]
+                        { quest = 82498 }, -- Worldsoul: Darkflame Cleft [H]
+                        { quest = 82506 }, -- Worldsoul: Darkflame Cleft [M]
+                        { quest = 82490 }, -- Worldsoul: Priory of the Sacred Flame [N]
+                        { quest = 82499 }, -- Worldsoul: Priory of the Sacred Flame [H]
+                        { quest = 82507 }, -- Worldsoul: Priory of the Sacred Flame [M]
+                        { quest = 82489 }, -- Worldsoul: The Dawnbreaker [N]
+                        { quest = 82493 }, -- Worldsoul: The Dawnbreaker [H]
+                        { quest = 82501 }, -- Worldsoul: The Dawnbreaker [M]
+                        { quest = 82486 }, -- Worldsoul: The Rookery [N]
+                        { quest = 82500 }, -- Worldsoul: The Rookery [H]
+                        { quest = 82508 }, -- Worldsoul: The Rookery [M]
+                        { quest = 82487 }, -- Worldsoul: The Stonevault [N]
+                        { quest = 82497 }, -- Worldsoul: The Stonevault [H]
+                        { quest = 82505 }, -- Worldsoul: The Stonevault [M]
+                        { quest = 82509 }, -- Worldsoul: Nerub-ar Palace [LFR]
+                        { quest = 82659 }, -- Worldsoul: Nerub-ar Palace [N]
+                        { quest = 82510 }, -- Worldsoul: Nerub-ar Palace [H]
+                    },
+                },
+                {
                     key = 'awakeningTheMachine',
                     minimumLevel = 70,
                     entries = {
@@ -70,48 +114,6 @@ Addon.data.chores.choresWarWithin = {
                         { quest = 83459 }, -- The Dawnbreaker
                         { quest = 83432 }, -- The Rookery
                         { quest = 83457 }, -- The Stonevault
-                    },
-                },
-                {
-                    key = 'worldsoul',
-                    minimumLevel = 70,
-                    chooseQuest = true,
-                    entries = {
-                        { quest = 82511 }, -- Worldsoul: Awakening Machine
-                        { quest = 82453 }, -- Worldsoul: Encore!
-                        { quest = 82458 }, -- Worldsoul: Renown
-                        { quest = 82516 }, -- Worldsoul: Severed Threads Pact
-                        { quest = 82482 }, -- Worldsoul: Snuffling
-                        { quest = 82483 }, -- Worldsoul: Spreading the Light
-                        { quest = 82512 }, -- Worldsoul: World Boss
-                        { quest = 82452 }, -- Worldsoul: World Quests
-                        { quest = 82491 }, -- Worldsoul: Ara-Kara, City of Echoes [N]
-                        { quest = 82494 }, -- Worldsoul: Ara-Kara, City of Echoes [H]
-                        { quest = 82502 }, -- Worldsoul: Ara-Kara, City of Echoes [M]
-                        { quest = 82485 }, -- Worldsoul: Cinderbrew Meadery [N]
-                        { quest = 82495 }, -- Worldsoul: Cinderbrew Meadery [H]
-                        { quest = 82503 }, -- Worldsoul: Cinderbrew Meadery [M]
-                        { quest = 82492 }, -- Worldsoul: City of Threads [N]
-                        { quest = 82496 }, -- Worldsoul: City of Threads [H]
-                        { quest = 82504 }, -- Worldsoul: City of Threads [M]
-                        { quest = 82488 }, -- Worldsoul: Darkflame Cleft [N]
-                        { quest = 82498 }, -- Worldsoul: Darkflame Cleft [H]
-                        { quest = 82506 }, -- Worldsoul: Darkflame Cleft [M]
-                        { quest = 82490 }, -- Worldsoul: Priory of the Sacred Flame [N]
-                        { quest = 82499 }, -- Worldsoul: Priory of the Sacred Flame [H]
-                        { quest = 82507 }, -- Worldsoul: Priory of the Sacred Flame [M]
-                        { quest = 82489 }, -- Worldsoul: The Dawnbreaker [N]
-                        { quest = 82493 }, -- Worldsoul: The Dawnbreaker [H]
-                        { quest = 82501 }, -- Worldsoul: The Dawnbreaker [M]
-                        { quest = 82486 }, -- Worldsoul: The Rookery [N]
-                        { quest = 82500 }, -- Worldsoul: The Rookery [H]
-                        { quest = 82508 }, -- Worldsoul: The Rookery [M]
-                        { quest = 82487 }, -- Worldsoul: The Stonevault [N]
-                        { quest = 82497 }, -- Worldsoul: The Stonevault [H]
-                        { quest = 82505 }, -- Worldsoul: The Stonevault [M]
-                        { quest = 82509 }, -- Worldsoul: Nerub-ar Palace [LFR]
-                        { quest = 82659 }, -- Worldsoul: Nerub-ar Palace [N]
-                        { quest = 82510 }, -- Worldsoul: Nerub-ar Palace [H]
                     },
                 },
                 {
