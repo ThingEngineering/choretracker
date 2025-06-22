d10586bdf5ce4e8f0f401c75ec341215928b9141
Crazyyoungs <34645867+Crazyyoungs@users.noreply.github.com>
2025-06-22 20:34:31 +0900

    Update koKR.lua

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -1,4 +1,4 @@
-local L = LibStub('AceLocale-3.0'):NewLocale('ChoreTracker', 'koKR')
+local L = LibStub('AceLocale-3.0'):NewLocale('ChoreTracker', 'koKR', false)
 if not L then return end -- don't remove this!
 
 L['section:dragonflight'] = EXPANSION_NAME9
