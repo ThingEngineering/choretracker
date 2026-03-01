0acef3bde09f1447e25d17d91e31497ad79ccf48
Freddie <freddie@thing.engineering>
2026-03-01 14:10:10 -0800

    Professions are annoying

--- Data/Professions.lua
+++ Data/Professions.lua
@@ -7,60 +7,84 @@ Addon.data.professionSkillLines = {
     [171] = {
         2823, -- Dragon Isles
         2871, -- Khaz Algar
+        2906, -- Midnight
     },
     -- Blacksmithing
     [164] = {
         2822, -- Dragon Isles
         2872, -- Khaz Algar
+        2907, -- Midnight
     },
     -- Enchanting
     [333] = {
         2825, -- Dragon Isles
         2874, -- Khaz Algar
+        2909, -- Midnight
     },
     -- Engineering
     [202] = {
         2827, -- Dragon Isles
         2875, -- Khaz Algar
+        2910, -- Midnight
     },
     -- Herbalism
     [182] = {
         2832, -- Dragon Isles
         2877, -- Khaz Algar
+        2912, -- Midnight
     },
     -- Inscription
     [773] = {
         2828, -- Dragon Isles
         2878, -- Khaz Algar
+        2913, -- Midnight
     },
     -- Jewelcrafting
     [755] = {
         2829, -- Dragon Isles
         2879, -- Khaz Algar
+        2914, -- Midnight
     },
     -- Leatherworking
     [165] = {
         2830, -- Dragon Isles
         2880, -- Khaz Algar
+        2915, -- Midnight
     },
     -- Mining
     [186] = {
         2833, -- Dragon Isles
         2881, -- Khaz Algar
+        2916, -- Midnight
     },
     -- Skinning
     [393] = {
         2834, -- Dragon Isles
         2882, -- Khaz Algar
+        2917, -- Midnight
     },
     -- Tailoring
     [197] = {
         2831, -- Dragon Isles
         2883, -- Khaz Algar
+        2918, -- Midnight
     },
 }
 
 Addon.data.skillLineSpell = {
+    -- Midnight
+    [2823] = 471003, -- Alchemy
+    [2822] = 471004, -- Blacksmithing
+    [2825] = 471006, -- Enchanting
+    [2827] = 471007, -- Engineering
+    [2832] = 471022, -- Herbalism
+    [2828] = 471010, -- Inscription
+    [2829] = 471011, -- Jewelcrafting
+    [2830] = 471012, -- Leatherworking
+    [2833] = 471013, -- Mining
+    [2834] = 471014, -- Skinning
+    [2831] = 471015, -- Tailoring
+
     -- Dragon Isles
     [2823] = 366261, -- Alchemy
     [2822] = 365677, -- Blacksmithing
