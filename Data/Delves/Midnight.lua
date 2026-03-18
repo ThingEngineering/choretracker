d79863f841f93fbe10adebc3bbddec95b29f198d
Freddie <freddie@thing.engineering>
2026-03-17 17:11:10 -0700

    Fix tracking quests for Midnight delves

--- Data/Delves/Midnight.lua
+++ Data/Delves/Midnight.lua
@@ -11,32 +11,32 @@ Addon.data.delves.midnight = {
         {
             uiMapId = 2395,
             pois = {
-                { active = 8426, inactive = 8425, quest = 0 }, -- Collegiate Calamity
-                { active = 8438, inactive = 8437, quest = 0 }, -- The Shadow Enclave
+                { active = 8426, inactive = 8425, quest = 91186 }, -- Collegiate Calamity
+                { active = 8438, inactive = 8437, quest = 91189 }, -- The Shadow Enclave
             },
         },
         -- Voidstorm
         {
             uiMapId = 2405,
             pois = {
-                { active = 8432, inactive = 8431, quest = 0 }, -- Shadowguard Point
-                { active = 8430, inactive = 8429, quest = 0 }, -- Sunkiller Sanctum
+                { active = 8432, inactive = 8431, quest = 91184 }, -- Shadowguard Point
+                { active = 8430, inactive = 8429, quest = 91183 }, -- Sunkiller Sanctum
             },
         },
         -- Harandar
         {
             uiMapId = 2413,
             pois = {
-                { active = 8434, inactive = 8433, quest = 0 }, -- The Grudge Pit
-                { active = 8436, inactive = 8435, quest = 0 }, -- The Gulf of Memory
+                { active = 8434, inactive = 8433, quest = 91185 }, -- The Grudge Pit
+                { active = 8436, inactive = 8435, quest = 91187 }, -- The Gulf of Memory
             },
         },
         -- Zul'Aman
         {
             uiMapId = 2437,
             pois = {
-                { active = 8444, inactive = 8443, quest = 0 }, -- Atal'Aman
-                { active = 8442, inactive = 8441, quest = 0 }, -- Twilight Crypts
+                { active = 8444, inactive = 8443, quest = 91188 }, -- Atal'Aman
+                { active = 8442, inactive = 8441, quest = 91190 }, -- Twilight Crypts
             },
         },
     },
